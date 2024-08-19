7cd674879bc588abcc811ec2294e9e7bbf56c456
Pablo Macuada <pablo.macuada@usach.cl>
2024-08-19 11:49:08 -0400

    Se agrega el websocket

---
+++ frontend/services/WebSocketService.js
@@ -0,0 +1,28 @@
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
+
+export default class WebSocketService {
+  constructor(token) {
+    const socketUrl = `http://localhost:8080/websocket?token=${token}`;
+    this.socket = new SockJS(socketUrl);
+    this.stompClient = Stomp.over(this.socket);
+  }
+
+  connect(callback) {
+    this.stompClient.connect({}, frame => {
+      this.stompClient.subscribe('/topic/notifications', message => {
+        callback(JSON.parse(message.body));
+      });
+    });
+  }
+
+  disconnect() {
+    if (this.stompClient !== null) {
+      this.stompClient.disconnect();
+    }
+  }
+
+  sendMessage(message) {
+    this.stompClient.send('/app/message', {}, JSON.stringify(message));
+  }
+}
